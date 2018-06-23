0444d44f7e58f6d3e2d488e6647a6632b45a1984
Gabriel Valle <gvalle91@gmail.com>
2018-06-23 13:18:28 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -28,9 +28,9 @@ class Scraper
     student_links.each do |link|
       if link.attribute("href").value.include?("twitter")
         student_hash[:twitter] = link.css("a").attribute("href").value
-      elsif link.css("a").attribute("href").value.include?("linkedin")
+      elsif link.attribute("href").value.include?("linkedin")
         student_hash[:linkedin] = link.css("a").attribute("href").value
-      elsif link.css("a").attribute("href").value.include?("github")
+      elsif link.attribute("href").value.include?("github")
         student_hash[:github] = link.css("a").attribute("href").value
       else
         student_hash[:blog] = link.css("a").attribute("href").value
